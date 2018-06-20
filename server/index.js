566c033753c90a6bf7060cfae37948b979216fc3
Alex <alex.r.clark21@gmail.com>
2018-06-20 08:17:58 -0600

    fixed messages

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
     , socket = require('socket.io')
 
-const port = 3333
 const app = express()
-app.use( express.static( `${__dirname}/../build` ))
+const port = 3333
+app.use(express.static(`${__dirname}/../build`))
+
+
 const io = socket(app.listen(port, () => console.log(`listening on port ${port}`)))
 
 io.on('connection', socket => {
@@ -12,11 +14,11 @@ io.on('connection', socket => {
     //...listen for events and emit to connected sockets
     // anything that happens inside this callback function that is not nested inside an event, will happen immediately on connection.
     socket.on('message sent', (data) => {
-        console.log(data)
-        data.user = this.id
-        io.emit('message dispatched', data)
+        console.log('data',data)
         // "data" represents incoming data from the client
         //...do things like perform logic, store data in db, join room, emit data to connected sockets, etc.
+        // data.user = this.id
+        io.emit('message dispatched', data)
         socket.on('disconnect', () => {
             console.log('User Disconnected');
         })
--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super(props);
     this.state={
       message:'',
-      messages:[]
+      messages:[],
+      userId :''
     }
     this.updateMessages = this.updateMessages.bind(this)
     this.setUserId = this.setUserId.bind(this)
@@ -18,8 +19,9 @@ class App extends Component {
   componentDidMount(){
     this.socket = io('/');// '/' represents the url to the server. since we are using a proxy, we can just use '/'
     this.socket.on('message dispatched', this.updateMessages)// these are socket event listeners
-    this.socket.on('welcome', this.setUserId)// this is coming from the server. 
+    this.socket.on('welcome', this.setUserId)// the user id is generated and sent from the server when a new page loads in.
   }
+
   updateMessages(message){
     const updatedMessages = this.state.messages.slice()
     updatedMessages.push(message)
@@ -27,13 +29,15 @@ class App extends Component {
       messages:updatedMessages
     })
   }
+
   setUserId(user){
     console.log(user)
     this.setState(user)
   }
   sendMessage(){
     this.socket.emit('message sent', {
-      message: this.state.message
+      message: this.state.message,
+      user: this.state.userId
     })
     this.setState({message:''})
   }
@@ -43,6 +47,7 @@ class App extends Component {
     })
   }
   render() {
+    console.log(this.state)
     const messages = this.state.messages.map((e,i) => {
       const styles = e.user === this.state.userID ? {alignSelf: "flex-end", backgroundColor: "#2d96fb", color: "white"} : {alignSelf: "flex-start", backgroundColor: "#e5e6ea"}
       return (
